f2b39d52a1aae4b5dce9e434772bb64594b48d10
Rudra Patel <patelrudra2003@gmail.com>
2022-05-27 20:50:59 -0500

    Consolidates main function

--- src/main.py
+++ src/main.py
@@ -6,10 +6,6 @@ import os
 center_letter = "w"
 satellite_letters = ["a", "m", "g", "r", "i", "n"]
 
-#final outputs
-possible_answers = []
-possible_panagrams = []
-
 
 #constants
 WORD_FILE_PATH = os.path.join(os.path.dirname(__file__), "english_words.txt")
@@ -17,16 +13,20 @@ MINIMUM_WORD_LENGTH = 4
 MAXIMUM_WORD_LENGTH = 19
 
 
-def main(center_letter: str, satellite_letters: list[str]) -> None:
+def main(center_letter: str, satellite_letters: list[str]) -> tuple[list[str], list[str]]:
+    """Given the center letter and satellite letters for the New York Times Spelling Bee Game, attempts to find all possible words and panagrams"""
+    #final outputs
+    possible_answers = []
+    possible_panagrams = []
+
     alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
     acceptable_letters = [center_letter] + satellite_letters
     unacceptable_letters = [letter for letter in alphabet if letter not in acceptable_letters]
 
-
     def is_valid_word(word: str) -> bool:
         """Returns True if a word is a valid guess and False otherwise"""
         #no punctuation or numbers
-        if "." in word or "-" in word or str(range(10)) in word:
+        if "." in word or "-" in word or "'" in word or str(range(10)) in word:
             return False
 
         #need center letter
@@ -62,9 +62,11 @@ def main(center_letter: str, satellite_letters: list[str]) -> None:
     for word in possible_answers:
         if all([letter in word for letter in acceptable_letters]):
             possible_panagrams.append(word)
+    
+    return possible_answers, possible_panagrams
 
 
 if __name__ == "__main__":
-    main(center_letter, satellite_letters)
+    possible_answers, possible_panagrams = main(center_letter, satellite_letters)
     print(f"The possible words are: {possible_answers}")
     print(f"The possible panagrams are: {possible_panagrams}")
